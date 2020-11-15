d6c6e09e320f9a43cf6bc89fc858d78c44e0139e
adamzielina <56132235+adamzielina@users.noreply.github.com>
2020-11-15 22:19:33 +0100

    Update Liniowa.m

--- aproksymacja/Liniowa.m
+++ aproksymacja/Liniowa.m
@@ -1,4 +1,4 @@
-function [a1,a2] = Liniowa(x,y)
+function [a1,a2] = Liniowa(x,y) %do zad4.m
 n = length(x);
 a1 = [n*(sum(x.*y)) - sum(x)*sum(y)] / [n*sum(x.^2) - sum(x)^2];
 a2 = [sum(x.^2)*sum(y) - sum(x)*sum(x.*y)] / [n*sum(x.^2) - sum(x)^2];
