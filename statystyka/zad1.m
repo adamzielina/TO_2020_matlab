26115a4411bf949c6f290440f816342420158aea
adamzielina <56132235+adamzielina@users.noreply.github.com>
2020-12-01 21:26:00 +0100

    Update zad1.m

--- statystyka/zad1.m
+++ statystyka/zad1.m
@@ -1,1 +1,12 @@
+clc; close all; clear;
 
+load hospital;
+
+
+SAge = mean(hospital.Age)
+SWeight = mean(hospital.Weight)
+SBP = mean(hospital.BloodPressure)
+
+OdAge = std(hospital.Age)
+OdWeight = std(hospital.Weight)
+OdBP = std(hospital.BloodPressure)
