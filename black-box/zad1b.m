60f4e90dff7b7b66d5f2af0a4eb55b6257d30751
adamzielina <56132235+adamzielina@users.noreply.github.com>
2020-12-15 13:33:21 +0100

    Create zad1b.m

---
+++ black-box/zad1b.m
@@ -0,0 +1,14 @@
+clc; close all; clear;
+
+j = 1;
+for i=120:-10:-50
+ [status,result]=system(char(strcat({'to1002.exe'},{' '},{num2str(i)})));
+ WYNIK(j)=str2num(result);
+ j=j+1;
+end
+
+plot(120:-10:-50, WYNIK, 'r')
+xlabel('WEJŚCIE')
+ylabel('WYNIK')
+grid on
+title ('Analiza pliku to1002 dla kroku 10')
